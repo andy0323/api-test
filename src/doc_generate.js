b614ca28b854fc6a492ec87d2adfe7e656c51819
alfred sang <i5ting@126.com>
2014-12-20 18:17:59 +0800

    test toc

--- src/doc_generate.js
+++ src/doc_generate.js
@@ -18,8 +18,7 @@ function doc_generate_with_markdown(pwd_dir, cb_succ, cb_fail) {
 	//
 	_to_markdown(pwd_dir, cb_succ, cb_fail);
 
-	var api_md_file = pwd_dir + '/' + GENERATE_MARKDOWN_FILE_NAME;
-	_to_html(api_md_file);
+
 }
 
 module.exports = doc_generate_with_markdown
@@ -92,28 +91,3 @@ function _to_markdown(pwd_dir, cb_succ, cb_fail){
 	parse_with_response_dir(response_dir, cb_succ, cb_fail);
 }
 
-
-function _to_html(source_file){
-	// todo
-	var source_file = source_file; //'/Users/sang/workspace/github/api-test/design.md';
-
-	//函数可以返回当前正在执行的项目路径
-	var pwd = process.cwd()  
-
-	var source_file_name = source_file;
-	var file_name = source_file_name.split('/').pop();;
-	var _file_name = file_name.split('.')[0];
-
-	var dest_file_path = pwd + '/preview/' + _file_name + '.html';
-
-	console.log('pwd=' + pwd);
-	console.log('source_file_name=' + source_file_name);
-	console.log('dest_file_path=' + dest_file_path);
-
-	var is_open = true;
-	var markd_config = {
-		debug: false
-	}
-	require('i5ting_toc')(pwd, source_file_name, dest_file_path, is_open, markd_config);
-	
-}
