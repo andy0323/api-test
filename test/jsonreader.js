5023e1eeec36c5cf2c895412c59eeffa523ecb07
alfred sang <i5ting@126.com>
2014-12-20 12:29:54 +0800

    add jsonreader && dir request

---
+++ test/jsonreader.js
@@ -0,0 +1,13 @@
+var jsonreader = require('../jsonreader');
+
+var file = '../play/request/get.req'
+// file = "./req.json"
+
+jsonreader(file, function(obj){
+	console.log("cb_succ")
+	
+	console.log(obj);
+	
+}, function(){
+	console.log("cb_fail")
+});
