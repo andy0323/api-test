a9848b6f05d55d8aa27a2460832af75c74e0072b
alfred sang <i5ting@126.com>
2014-12-20 21:09:39 +0800

    add bluebird

--- src/jsonreader.js
+++ src/jsonreader.js
@@ -1,20 +1,25 @@
-var fs = require('fs');
+// var fs = require('fs');
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+
 
 function read_json(file, cb_succ, cb_fail) {
-	function parseJson (file, er, content, cb_succ, cb_fail) {
-	  try {
-	    d = JSON.parse((content))
-			d.file_name = file.split('/').pop();;
-			d.file_path = file;
-			cb_succ(d);
-	  } catch (er) {
-			cb_fail();
-	  }
-	}
-
-  fs.readFile(file, "utf8", function (err, content) {
-    parseJson(file, err, content, cb_succ, cb_fail)
-  })
+	// function parseJson (file, er, content, cb_succ, cb_fail) {
+	//   try {
+	//     d = JSON.parse((content))
+	// 		d.file_name = file.split('/').pop();;
+	// 		d.file_path = file;
+	// 		cb_succ(d);
+	//   } catch (er) {
+	// 		cb_fail();
+	//   }
+	// }
+	//
+	//   fs.readFileSync(file, "utf8", function (err, content) {
+	//     parseJson(file, err, content, cb_succ, cb_fail)
+	//   })
+	//
+	return fs.readFileAsync(file).then(JSON.parse);
 }
 
 module.exports = read_json
